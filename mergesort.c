60c75b1f3f8a402e440546102b8fedef4d68746e
Aswin Jayaji <56126732+aswinjayaji@users.noreply.github.com>
2020-10-05 02:54:02 -0700

    Changed all i ,j to index1,index2

--- mergesort.c
+++ mergesort.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
 void main()
 {
-  int a[25],b[25],c[50],ano,bno,i,j;
+  int a[25],b[25],c[50],ano,bno,index,index2;
   printf("Enter the number of elements in the first array:");
   scanf("%d",&ano);
-  for(i=0;i<ano;i++)
-    scanf("%d",&a[i]);
+  for(index=0;index<ano;index++)
+    scanf("%d",&a[index]);
   printf("Enter the no. of elements in the second array:");
   scanf("%d",&bno);
-  for(i=0;i<bno;i++)
-    scanf("%d",&b[i]);
-  for(i=0,j=0;i<ano&&j<bno;)
+  for(index=0;index<bno;index++)
+    scanf("%d",&b[index]);
+  for(index=0,index2=0;index<ano&&index2<bno;)
   {
-    if(a[i]<b[j])
+    if(a[index]<b[index2])
     {
-      c[i+j]=a[i];
-      i=i++;
+      c[index+index2]=a[index];
+      index=index++;
     }
     else
     {
-      c[i+j]=b[j];
-      j=j++;
+      c[index+index2]=b[index2];
+      index2=index2++;
     }
   }
-  for(;i<ano;i++)
-    c[i+j]=a[i];
-  for(;j<bno;j++)
-    c[i+j]=b[j];
+  for(;index<ano;index++)
+    c[index+index2]=a[index];
+  for(;index2<bno;index2++)
+    c[index+index2]=b[index2];
   printf("The newly merged array is:\n");
-  for(int q=0;q<i+j;q++)
+  for(int q=0;q<index+index2;q++)
     printf("%d\t",c[q]);
 }
